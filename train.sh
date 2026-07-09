7f03b300cf1f649ebf6ee3cd19234d42fcc0f16f
xyywsx <3232510137@qq.com>
2026-07-09 10:25:00 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,2 @@
+#CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 opengait/main.py --cfgs ./configs/smplgait/smplgaitgl.yaml --phase train
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 opengait/main.py --cfgs ./configs/gaitmbgl/gaitmbgl.yaml --phase train
